0a335272a2bf55f17010b220c13195089976f7a2
doraneko94 <shuntaro94@gmail.com>
2020-10-05 13:21:27 +0900

    cargo fmt

--- src/linalg.rs
+++ src/linalg.rs
@@ -1,6 +1,6 @@
 use ndarray::*;
 use num_traits::{Float, FromPrimitive};
-use rand::distributions::{Distribution, Uniform, uniform::SampleUniform};
+use rand::distributions::{uniform::SampleUniform, Distribution, Uniform};
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
@@ -22,7 +22,11 @@ impl<T: Float> Padded2D<T> {
         let shape = a.shape();
         let raw_size = (shape[0], shape[1]);
         let data = a.padding_zero(padding_size);
-        Self { data, raw_size, padding_size }
+        Self {
+            data,
+            raw_size,
+            padding_size,
+        }
     }
 
     pub fn raw_view(&self) -> ArrayView2<T> {
@@ -31,13 +35,15 @@ impl<T: Float> Padded2D<T> {
 
     pub fn shift_view(&self, l: usize, shift_dir: ShiftDirect) -> Result<ArrayView2<T>, &str> {
         let padding_size = self.padding_size;
-        if padding_size < l { return Err("Shitf size l is larger than padding size!"); }
+        if padding_size < l {
+            return Err("Shitf size l is larger than padding size!");
+        }
         let raw_size = self.raw_size;
         let start = match shift_dir {
             ShiftDirect::Left => padding_size + l,
             ShiftDirect::Right => padding_size - l,
         };
-        Ok(self.data.slice(s![.., start..start+raw_size.1]))
+        Ok(self.data.slice(s![.., start..start + raw_size.1]))
     }
 }
 
@@ -48,7 +54,7 @@ pub trait Padding2D {
 
 impl<T> Padding2D for Array2<T>
 where
-    T: Float
+    T: Float,
 {
     // padding 2D array
     fn padding_zero(&self, padding_size: usize) -> Self {
@@ -60,7 +66,7 @@ where
     fn into_padding_zero(&mut self, padding_size: usize) {
         let shape = self.shape();
         let mut a: Array2<T> = Array2::zeros((shape[0], shape[1] + padding_size * 2));
-        let mut a_smut = a.slice_mut(s![.., padding_size..padding_size+shape[1]]);
+        let mut a_smut = a.slice_mut(s![.., padding_size..padding_size + shape[1]]);
         Zip::from(&mut a_smut).and(&self.to_owned()).apply(|a, &b| {
             *a = b;
         });
@@ -70,7 +76,7 @@ where
 
 pub trait Tensor3D<T>
 where
-    T: Float + std::ops::AddAssign + 'static
+    T: Float + std::ops::AddAssign + 'static,
 {
     fn convolution(&self, rhs: &Padded2D<T>) -> Array2<T>;
     fn transpose_convolution(&self, rhs: &Padded2D<T>) -> Array2<T>;
@@ -78,7 +84,7 @@ where
 
 impl<T> Tensor3D<T> for Array3<T>
 where
-    T: Float + std::ops::AddAssign + std::ops::MulAssign + 'static
+    T: Float + std::ops::AddAssign + std::ops::MulAssign + 'static,
 {
     fn convolution(&self, rhs: &Padded2D<T>) -> Array2<T> {
         let shape3d = self.shape();
@@ -87,7 +93,9 @@ where
         let t = rhs.raw_size.1;
         let mut arr = Array2::zeros((n, t));
         for i in 0..l {
-            let brr = self.slice(s![.., .., i]).dot(&rhs.shift_view(i, ShiftDirect::Right).unwrap());
+            let brr = self
+                .slice(s![.., .., i])
+                .dot(&rhs.shift_view(i, ShiftDirect::Right).unwrap());
             Zip::from(&mut arr).and(&brr).apply(|a, &b| {
                 *a += b;
             });
@@ -102,14 +110,17 @@ where
         let t = rhs.raw_size.1;
         let mut arr = Array2::zeros((k, t));
         for i in 0..l {
-            let brr = self.slice(s![.., .., i]).t().dot(&rhs.shift_view(i, ShiftDirect::Left).unwrap());
+            let brr = self
+                .slice(s![.., .., i])
+                .t()
+                .dot(&rhs.shift_view(i, ShiftDirect::Left).unwrap());
             Zip::from(&mut arr).and(&brr).apply(|a, &b| {
                 *a += b;
             });
         }
         for i in 1..l {
-            Zip::from(&mut arr.slice_mut(s![.., t-i])).apply(|a| {
-                *a *= T::from(l).unwrap() / T::from(i).unwrap(); 
+            Zip::from(&mut arr.slice_mut(s![.., t - i])).apply(|a| {
+                *a *= T::from(l).unwrap() / T::from(i).unwrap();
             })
         }
         arr
@@ -121,9 +132,9 @@ pub trait Smooth {
     fn smooth_x(rhs: &Self, l: usize) -> Self;
 }
 
-impl<T> Smooth for Array2<T> 
+impl<T> Smooth for Array2<T>
 where
-    T: Float + Clone
+    T: Float + Clone,
 {
     fn smooth(&self, l: usize) -> Self {
         let shape = self.shape();
@@ -131,7 +142,7 @@ where
         let cols = shape[1];
         let mut arr = Array2::zeros((rows, cols));
         let mut brr: Array1<T> = Array1::zeros(rows);
-        for i in 0..l-1 {
+        for i in 0..l - 1 {
             brr = brr + self.slice(s![.., i]);
         }
         for i in 0..cols {
@@ -142,9 +153,11 @@ where
             if i >= l {
                 brr = brr - self.slice(s![.., i - l]);
             }
-            Zip::from(&mut arr.slice_mut(s![.., i])).and(&brr).apply(|a, &b| {
-                *a = b;
-            });
+            Zip::from(&mut arr.slice_mut(s![.., i]))
+                .and(&brr)
+                .apply(|a, &b| {
+                    *a = b;
+                });
         }
         arr
     }
@@ -155,7 +168,7 @@ where
         let cols = shape[1];
         let mut arr = Array2::zeros((rows, cols));
         let mut brr: Array1<T> = Array1::zeros(cols);
-        for i in 0..l-1 {
+        for i in 0..l - 1 {
             brr = brr + rhs.slice(s![i, ..]);
         }
         for i in 0..rows {
@@ -166,9 +179,11 @@ where
             if i >= l {
                 brr = brr - rhs.slice(s![i - l, ..]);
             }
-            Zip::from(&mut arr.slice_mut(s![i, ..])).and(&brr).apply(|a, &b| {
-                *a = b;
-            });
+            Zip::from(&mut arr.slice_mut(s![i, ..]))
+                .and(&brr)
+                .apply(|a, &b| {
+                    *a = b;
+                });
         }
         arr
     }
@@ -202,12 +217,16 @@ impl<T: Float + SampleUniform> RandomArray for Array2<T> {
         let mut arr = Array2::zeros((y_size, x_size));
         for y in 0..y_size {
             let i = ud.sample(&mut rng);
-            Zip::from(&mut arr.slice_mut(s![y, i..])).and(&self.slice(s![y, ..x_size-i])).apply(|a, &b| {
-                *a = b;
-            });
-            Zip::from(&mut arr.slice_mut(s![y, ..i])).and(&self.slice(s![y, x_size-i..])).apply(|a, &b| {
-                *a = b;
-            });
+            Zip::from(&mut arr.slice_mut(s![y, i..]))
+                .and(&self.slice(s![y, ..x_size - i]))
+                .apply(|a, &b| {
+                    *a = b;
+                });
+            Zip::from(&mut arr.slice_mut(s![y, ..i]))
+                .and(&self.slice(s![y, x_size - i..]))
+                .apply(|a, &b| {
+                    *a = b;
+                });
         }
         arr
     }
@@ -237,12 +256,16 @@ impl<T: Float + SampleUniform> RandomArray for Array3<T> {
         for ki in 0..k {
             for ni in 0..n {
                 let i = ud.sample(&mut rng);
-                Zip::from(&mut arr.slice_mut(s![ni, ki, i..])).and(&self.slice(s![ni, ki, ..l-i])).apply(|a, &b| {
-                    *a = b;
-                });
-                Zip::from(&mut arr.slice_mut(s![ni, ki, ..i])).and(&self.slice(s![ni, ki, l-i..])).apply(|a, &b| {
-                    *a = b;
-                });
+                Zip::from(&mut arr.slice_mut(s![ni, ki, i..]))
+                    .and(&self.slice(s![ni, ki, ..l - i]))
+                    .apply(|a, &b| {
+                        *a = b;
+                    });
+                Zip::from(&mut arr.slice_mut(s![ni, ki, ..i]))
+                    .and(&self.slice(s![ni, ki, l - i..]))
+                    .apply(|a, &b| {
+                        *a = b;
+                    });
             }
         }
         arr
@@ -277,11 +300,11 @@ pub trait OrdArray<T: Clone, D: RemoveAxis> {
 
 impl<T: Float, D: RemoveAxis> OrdArray<T, D> for Array<T, D> {
     fn max_axis(&self, axis: Axis) -> Array<T, D::Smaller> {
-        self.fold_axis(axis, T::neg_infinity(), |&m, &x| if m < x { x } else { m } )
+        self.fold_axis(axis, T::neg_infinity(), |&m, &x| if m < x { x } else { m })
     }
 
     fn min_axis(&self, axis: Axis) -> Array<T, D::Smaller> {
-        self.fold_axis(axis, T::infinity(), |&m, &x| if m > x { x } else { m } )
+        self.fold_axis(axis, T::infinity(), |&m, &x| if m > x { x } else { m })
     }
 }
 
--- src/update.rs
+++ src/update.rs
@@ -4,14 +4,21 @@ use num_traits::Float;
 use crate::linalg::*;
 
 pub fn update_wh<T>(
-    k: usize, l: usize, lambda: T,
-    w: &mut Array3<T>, h: &mut Array2<T>, x_bar: &mut Array2<T>, wtx: &mut Array2<T>,
-    x: &Array2<T>, x_pad: &Padded2D<T>,
-    cost: &mut Vec<T>, epsilon: T,
-    update_w: bool, update_h: bool,
-) 
-where
-    T: Float + std::ops::AddAssign + ScalarOperand + std::ops::MulAssign + 'static
+    k: usize,
+    l: usize,
+    lambda: T,
+    w: &mut Array3<T>,
+    h: &mut Array2<T>,
+    x_bar: &mut Array2<T>,
+    wtx: &mut Array2<T>,
+    x: &Array2<T>,
+    x_pad: &Padded2D<T>,
+    cost: &mut Vec<T>,
+    epsilon: T,
+    update_w: bool,
+    update_h: bool,
+) where
+    T: Float + std::ops::AddAssign + ScalarOperand + std::ops::MulAssign + 'static,
 {
     if !update_w && !update_h {
         return;
@@ -30,14 +37,23 @@ where
     let l_sh_one_eye = Array2::smooth_x(&h.t().dot(&one_eye), l) * lambda;
     if update_w {
         for i in 0..l {
-            let hlt = h_pad.shift_view(i, ShiftDirect::Right).unwrap().t().to_owned();
+            let hlt = h_pad
+                .shift_view(i, ShiftDirect::Right)
+                .unwrap()
+                .t()
+                .to_owned();
             let n0 = x.dot(&hlt);
             let d0 = x_bar.dot(&hlt);
-            let d1 = x_pad.shift_view(i, ShiftDirect::Right).unwrap().dot(&l_sh_one_eye);
+            let d1 = x_pad
+                .shift_view(i, ShiftDirect::Right)
+                .unwrap()
+                .dot(&l_sh_one_eye);
             let dw1 = n0 / (d0 + d1 + epsilon);
-            Zip::from(&mut w_new.slice_mut(s![.., .., i])).and(&dw1).apply(|a, &b| {
-                *a *= b;
-            });
+            Zip::from(&mut w_new.slice_mut(s![.., .., i]))
+                .and(&dw1)
+                .apply(|a, &b| {
+                    *a *= b;
+                });
         }
     }
     if update_h {
